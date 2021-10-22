126074a09c09a39196ef4ec1c5fdabfb8e85e8c5
Himanshu Pal <himanshu.p19@iiits.in>
2021-10-22 22:28:24 +0530

    adding route for short Urls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const shortUrl = require("./models/shortUrl");
+const ShortUrl = require("./models/shortUrl");
 
 mongoose.connect("mongodb://localhost/urlShortner");
 
@@ -10,13 +10,24 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-  const shortUrls = await shortUrl.find();
+  const shortUrls = await ShortUrl.find();
   res.render("index", { shortUrls: shortUrls });
 });
 
 app.post("/shortUrls", async (req, res) => {
-  await shortUrl.create({ full: req.body.fullUrl });
+  await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 5000);
